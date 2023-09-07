d95b9d34cb2ba15bc177275503c3a3db5517f8bd
Lena <lenazozula05@gmail.com>
2023-09-07 23:35:39 +0300

    Добавьте файлы проекта.

---
+++ Share/SocketCommon.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Threading;
+
+namespace Share
+{
+    public delegate void LogUIDelegate(string message);
+
+    public class SocketCommon
+    {
+        public LogUIDelegate? logFunction;
+        protected readonly SynchronizationContext? syncContext = SynchronizationContext.Current;
+
+        protected virtual void Log(string message)
+        {
+            if(logFunction != null)
+            {
+                if(syncContext == SynchronizationContext.Current)
+                {
+                    logFunction(message);
+                }
+                else
+                {
+                    syncContext?.Post(x => logFunction(message), null);
+                }
+            }
+        }
+
+        protected virtual string AddLogHeader(IPEndPoint socket)
+        {
+            return "/" + socket.Address.ToString() + "/" + socket.Port.ToString() + "/";
+        }
+    }
+}
